1f2d30d3cd5cbdd2446c6dd716c5b322340e9fe4
Amol derick soans <amoldericksoans@gmail.com>
2025-01-26 23:30:32 +0530

    added signout route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
     <ClerkProvider
     appearance={{
       baseTheme: shadesOfPurple,
-    }}>
+    }}
+    afterSignOutUrl="/"
+    >
       <html lang="en">
         <body className={inter.className}>
           {children}
